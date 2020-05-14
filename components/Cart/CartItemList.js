942757af4bcc3e7070acbabdff2782ef4025fdde
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-05-14 15:27:12 -0400

    cart page design

--- components/Cart/CartItemList.js
+++ components/Cart/CartItemList.js
@@ -1,5 +1,27 @@
+import { Header, Segment, Button, Icon } from 'semantic-ui-react'
+
 function CartItemList() {
-  return <>CartItemList</>;
+  const user = false;
+  
+  return (
+    <Segment secondary color='teal' inverted textAlign='center' placeholder>
+      <Header icon>
+        <Icon name='shopping basket'/>
+        No Products In Cart
+      </Header>
+      <div>
+        {user ? (
+          <Button color='orange'>
+            View Products
+          </Button>
+        ) : (
+          <Button color='blue'>
+            Log In To Add Products
+          </Button>
+        )}
+      </div>
+    </Segment>
+  )
 }
 
 export default CartItemList;
