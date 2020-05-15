20b379ddcf1e4ebcf428091af5d2cc4cacf95d9e
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-05-15 12:00:39 -0400

    building signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,83 @@
+import React from "react";
+import { Button, Form, Icon, Message, Segment } from "semantic-ui-react";
+import Link from "next/link";
+
+const INITIAL_USER = {
+  name: "",
+  email: "",
+  password: "",
+};
+
 function Signup() {
-  return <>signup</>;
+  const [user, setUser] = React.useState(INITIAL_USER);
+  const [disabled, setDisabled] = React.useState(true)
+
+  React.useEffect(() => {
+    const isUser = Object.values(user).every(el => Boolean(el))
+    isUser ? setDisabled(false) : setDisabled(true);
+  }, [user])
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setUser((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  return (
+    <>
+      <Message
+        attached
+        icon="settings"
+        header="Get Started!"
+        content="Create A New Account"
+        color="teal"
+      />
+      <Form>
+        <Segment>
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            label="Name"
+            placeholder="Name"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            label="Email"
+            placeholder="Email"
+            name="email"
+            type="email"
+            value={user.email}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            label="Password"
+            placeholder="Password"
+            name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
+          />
+          <Button disabled={disabled} icon="signup" type="submit" color="orange" content="Signup" />
+        </Segment>
+      </Form>
+      <Message attatched="bottom" warning>
+        <Icon nam="help" />
+        Existing User?{" "}
+        <Link href="/login">
+          <a>Log In Here</a>
+        </Link>{" "}
+        Instead.
+      </Message>
+    </>
+  );
 }
 
 export default Signup;
