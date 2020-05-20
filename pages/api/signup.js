df4d585ec5866559906282a7aeba2fa9d07b54d0
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-05-20 14:25:52 -0400

    seeting up sign in

---
+++ pages/api/signup.js
@@ -0,0 +1,13 @@
+import connectDb from '../../utils/connectDb'
+import User from '../../models/User'
+
+connectDb()
+
+export default async (req, res) => {
+    const { name, email, password } = req.body
+    try {
+
+    } catch(error) {
+
+    }
+}
