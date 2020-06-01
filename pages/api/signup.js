bbc8808a5f5048c9abf3ddd052605b2726a2d449
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-06-01 11:45:43 -0400

    Working Signup Errors

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,12 +2,23 @@ import connectDb from '../../utils/connectDb'
 import User from '../../models/User'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import isEmail from 'validator/lib/isEmail'
+import isLength from 'validator/lib/isLength'
 
 connectDb()
 
 export default async (req, res) => {
     const { name, email, password } = req.body
     try {
+
+        if (!isLength(name, { min: 3, max: 20 })) {
+            return res.status(422).send('Name Must Be 3-20 Characters')
+        } else if (!isLength(password, { min:6 })) {
+            return res.status(422).send('Password Must Be At Least 6 Characters')
+        } else if (!isEmail(email)) {
+            return res.status(422).send('Please Provide Valid Email')
+        }
+
         const user = await User.findOne({ email })
         if (user) {
             return res.status(422).send(`User Already Exists With Email ${email}`)
