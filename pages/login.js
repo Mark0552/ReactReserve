af4047c01e7f8040f798338d5dffcf59832854d0
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-06-03 13:30:48 -0400

    login error messages, login web token working

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Button, Form, Icon, Message, Segment } from "semantic-ui-react";
 import Link from "next/link";
+import axios from 'axios'
 import catchErrors from '../utils/catchErrors'
+import baseUrl from '../utils/baseUrl'
+import { handleLogin } from '../utils/auth'
 
 const INITIAL_USER = {
   email: "",
@@ -24,14 +27,17 @@ function Signup() {
     setUser((prevState) => ({ ...prevState, [name]: value }));
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault()
 
     try {
       setLoading(true)
       setError('')
-      console.log(user)
-    } catch(error) {
+      const url = `${baseUrl}/api/login`
+      const payload = { ...user }
+      const response = await axios.post(url, payload)
+      handleLogin(response.data)
+    } catch (error) {
       catchErrors(error, setError)
     } finally {
       setLoading(false)
