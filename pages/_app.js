22685976155fee39cff523c1aa67013effa6f5d3
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-07-21 15:54:38 -0400

    added managing invalid tokens

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from "next/app";
 import Layout from '../components/_App/Layout'
-import { parseCookies } from 'nookies'
+import { parseCookies, destroyCookie } from 'nookies'
 import { redirectUser } from '../utils/auth'
 import baseUrl from '../utils/baseUrl'
 import axios from 'axios'
@@ -31,6 +31,10 @@ class MyApp extends App {
         pageProps.user = user;
       } catch (error) {
         console.error('Error Getting Current User', error);
+        // Throw out Invalid Token
+        destroyCookie(ctx, 'token')
+        // Redirect to Login Page
+        redirectUser(ctx, '/login')
 
       }
     }
