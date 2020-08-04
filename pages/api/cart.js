f18cfc5a24139082a821ad3b3c76092d6f52a5a3
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-08-03 23:19:31 -0400

    adding products to cart in database only working

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -3,9 +3,25 @@ import jwt from 'jsonwebtoken'
 import Cart from '../../models/Cart'
 import connectDb from '../../utils/connectDb'
 
-connectDb()
+connectDb();
+
+const { ObjectId } = mongoose.Types
 
 export default async (req, res) => {
+    switch (req.method) {
+        case 'GET':
+            await handleGetRequest(req, res)
+            break;
+        case 'PUT':
+            await handlePutRequest(req, res);
+            break;
+        default:
+            res.status(405).send(`Method ${req.method} Not Allowed`)
+            break;
+    }
+}
+
+async function handleGetRequest (req, res) {
     if (!('authorization' in req.headers)) {
         return res.status(401).send('No Authorization Token')
     }
@@ -17,6 +33,38 @@ export default async (req, res) => {
         })
         res.status(200).json(cart.products)
     } catch (error) {
+        console.error(error)
+        res.status(403).send('Please Login Again')
+    }
+}
+
+async function handlePutRequest(req, res) {
+    const { quantity, productId } = req.body
+    if (!('authorization' in req.headers)) {
+        return res.status(401).send('No Authorization Token')
+    }
+    try {
+        const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+        // get user cart based on user id
+        const cart = await Cart.findOne({ user: userId })
+        // check if product already exists in cart
+        const productExists = cart.products.some(doc => ObjectId(productId).equals(doc.product))
+        // if so increment quantity by number provided
+        if (productExists) {
+            await Cart.findOneAndUpdate(
+                { _id: cart._id, 'products.product': productId },
+                { $inc: { 'products.$.quantity': quantity } }
+            )
+        } else {
+            // if not add new product with given quantity
+            const newProduct = { quantity, product: productId }
+            await Cart.findOneAndUpdate(
+                { _id: cart._id },
+                { $addToSet: { products: newProduct } }
+            )
+        }
+        res.status(200).send('Cart Updated')
+    } catch {
         console.error(error)
         res.status(403).send('Please Login Again')
     }
