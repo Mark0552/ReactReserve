015ac3f6630599215c90d4c1ac315ac9442d9b0d
Mark0552 <50115357+Mark0552@users.noreply.github.com>
2020-08-12 14:29:32 -0400

    deployment phase

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === 'production' 
-    ? 'https://deployment-url.now.sh'
+    ? 'https://marks-showcase-app.now.sh'
     : 'http://localhost:3000';
 
     export default baseUrl;
